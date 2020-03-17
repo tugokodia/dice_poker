815bd22bb411800ce9ac2d570e92444f745c1be2
tugokodia <tugokodia@gmail.com>
2020-03-17 08:13:49 +0300

    add 2 players game mod

--- dice_poker.py
+++ dice_poker.py
@@ -1,20 +1,96 @@
-# TODO: класс игрока, компа наследовать, сохранения 
 import random
 import time, os
 from colorama import Fore, Style
 from key_listener import getch
-player = [0, 0, 0, 0, 0]
-ai = [0, 0, 0, 0, 0]
+
+class Player:
     
-def throw_dices():
-    dices = []
-    for n in range(5):        
-        rnd = random.randrange(1, 7)
-        dices.append(rnd)
-    dices.sort()
-    return dices
+    def __init__(self, name = 'player', money = 100):
+        
+        self.name = name
+        self.money = money
+        self.dices = [0, 0, 0, 0, 0]
+        self.score = 0
+        
+    
+    def do_turn(self):
+        
+        rer = input("input a,b,c,d,e to reroll dices: ")
+        
+        if len(rer) > 0:        
+            if 'a' in rer:
+                self.dices[0] = random.randrange(1, 7)
+            if 'b' in rer:
+                self.dices[1] = random.randrange(1, 7)
+            if 'c' in rer:
+                self.dices[2] = random.randrange(1, 7)
+            if 'd' in rer:
+                self.dices[3] = random.randrange(1, 7)
+            if 'e' in rer:
+                self.dices[4] = random.randrange(1, 7)        
+        
+    
+    def do_bet(self):        
+        
+        bet = 0
+        
+        while True:
+            
+            print("Type bet from 1 to 5")
+            
+            try:
+                bet = int(input("Your bet? "))
+                
+            except ValueError:
+                print("Wrong number")
+                continue
+                
+            if bet < 1 or bet > 5:
+                print("From one to five. Type again")
+                continue
+            else:
+                break
+                
+        return bet
+            
+    
+    def throw_dices(self):
+    
+        dices = []
+        for n in range(5):        
+            rnd = random.randrange(1, 7)
+            dices.append(rnd)
+        dices.sort()
+        self.dices = dices
+        
+class Bot(Player):
+    
+    def do_turn(self):
+                
+        dct={}
+        index_to_roll=[]
+        for i in self.dices:
+            if i in dct:
+                dct[i] += 1
+            else:
+                dct[i] = 1    
+        for key in dct:
+            if dct[key] == 1:
+                for i,x in enumerate(self.dices):
+                    if x == key:                    
+                        index_to_roll.append(i)    
+        for i in index_to_roll:                
+            self.dices[i] = random.randrange(1, 7)        
+                        
+        
+    def do_bet(self):
+        
+        bet = 5
+        return bet
+            
 
 def help():
+    
     print(
 '''
 Покер ----------> все пять костей с одинаковым количеством очков.
@@ -26,58 +102,46 @@ def help():
 Две пары -------> две пары костей одного достоинства каждая.
 Пара -----------> две кости одного достоинства.
 ''')
-    print('\n\nPress enter to continue')
+    print("\n\nPress enter to continue")
     input()
     return ' '
 
 
 def start_menu():
-    print('1. Начать')
-    print('2. Инструкция')
+    
+    print("1. One player")
+    print("2. Two player\n")
+    print("5. Instructions\n")
+    print("0. Exit")
     
     while True:
+
         char = getch()
-        if (char == "1"):
-            player_money = 100
-            ai_money = 100
-            new_game(player_money, ai_money)
+        
+        if (char == '1'):       
+        
+            player_one = Player(input("Your name? "), 100)
+            player_two = Bot('cpu', 100)
+            new_game(player_one, player_two)
+        
+        elif (char == '2'):
+        
+            player_one = Player(input("Player one name? "), 100)
+            player_two = Player(input("Player two name? "), 100)
+            new_game(player_one, player_two)
             
-        if (char == "2"):
+        elif (char == '5'):
+            
             print(help())
-            start_menu()
+            start_menu()                 
             
-def get_bet():
-    bet = 0
-    while True:
-        print('Введите ставку от 1 до 5')
-        try:
-            bet = int(input('Ваша ставка? '))
-        except ValueError:
-            print('Введена неверная сумма')
-            continue
-        if bet < 1 or bet > 5:
-            print('Нарушены приделы возможной ставки')
-            continue
-        else:
-            break
-    return bet
-    
-def reroll__player_dices(dices):    
-    rer = input('input a,b,c,d,e to reroll dices: ')
-    if len(rer) > 0:        
-        if 'a' in rer:
-            dices[0] = random.randrange(1, 7)
-        if 'b' in rer:
-            dices[1] = random.randrange(1, 7)
-        if 'c' in rer:
-            dices[2] = random.randrange(1, 7)
-        if 'd' in rer:
-            dices[3] = random.randrange(1, 7)
-        if 'e' in rer:
-            dices[4] = random.randrange(1, 7)
-    return dices
+        elif (char == '0'):
+            print("See ya!")
+            exit(0)
+
 
 def check_comb(player):
+    
     player.sort()    
     dct = {}
     comb_name = ''
@@ -117,77 +181,85 @@ def check_comb(player):
     if player == low_straight:
         comb_name = 'Low straight'
         scores = 500
-    return comb_name, scores
+    return comb_name, scores  
+  
     
-def reroll_ai_dices(dices):
-    dct={}
-    index_to_roll=[]
-    for i in dices:
-        if i in dct:
-            dct[i] += 1
-        else:
-            dct[i] = 1    
-    for key in dct:
-        if dct[key] == 1:
-            for i,x in enumerate(dices):
-                if x == key:                    
-                    index_to_roll.append(i)    
-    for i in index_to_roll:                
-        dices[i] = random.randrange(1, 7)
-    return dices                
+def new_game(player_one, player_two):
     
-def new_game(player_money, ai_money):
     os.system('clear')    
     bank = 0
-    ai_win = 0
-    player_win = 0
+    player_one_win = 0
+    player_two_win = 0
+    
     print(f'{Fore.YELLOW}################################################################################{Style.RESET_ALL}')
+    
     for _round in range(3):     
-        bet = get_bet()
-        player_money -= bet
-        ai_money -= bet
+        bet = player_one.do_bet()        
+        player_one.money = player_one.money - bet
+        player_two.money = player_two.money - bet
         bank = bank + bet
-        print("БАНК: ", bank)
-        print("Противник: ", ai_money)
-        print("Игрок: ", player_money)
-        print("Раунд: ", _round + 1)
-        print(f'{Fore.YELLOW}################################################################################{Style.RESET_ALL}')
-        print(f'{Fore.BLUE}PLAYER{Style.RESET_ALL}')
-        player = throw_dices()        
-        ai = throw_dices()
+        
+        player_win = 0
+        ai_win = 0
+        
+        print("Bank: ", bank)        
+        print(player_one.name,": ", player_one.money)
+        print(player_two.name,": ", player_two.money)
+        print("Round: ", _round + 1)
+        player_one.throw_dices()        
+        player_two.throw_dices()
+        
+        print(f"{Fore.YELLOW}################################################################################{Style.RESET_ALL}")
+        print(f"{Fore.BLUE}{player_one.name}{Style.RESET_ALL}")        
         print("[a, b, c, d, e]")
-        player_comb, player_score = check_comb(player)
-        print(player, " " ,player_comb, "\n")
-        player = reroll__player_dices(player)
-        player_comb, player_score = check_comb(player)
-        print(player, " ", player_comb, "\n")
-        print(f'{Fore.YELLOW}################################################################################{Style.RESET_ALL}')
-        print(f'{Fore.RED}AI{Style.RESET_ALL}')
-        ai_comb, ai_score = check_comb(ai)
-        print(ai, " ", ai_comb, "\n")
-        ai = reroll_ai_dices(ai)
-        ai_comb, ai_score = check_comb(ai)
-        print(ai, " ", ai_comb, "\n")
-        if player_score > ai_score:
+        p_one_comb, player_one.score = check_comb(player_one.dices)
+        print(player_one.dices, " " ,p_one_comb, "\n")
+        player_one.do_turn()
+        p_one_comb, player_one.score = check_comb(player_one.dices)
+        print(player_one.dices, " ", p_one_comb, "\n")
+        
+        print(f"{Fore.YELLOW}################################################################################{Style.RESET_ALL}")        
+        print(f"{Fore.RED}{player_two.name}{Style.RESET_ALL}")
+        print("[a, b, c, d, e]")
+        p_two_comb, player_two.score = check_comb(player_two.dices)
+        print(player_two.dices, " ", p_two_comb, "\n")
+        player_two.do_turn()
+        p_two_comb, player_two.score = check_comb(player_two.dices)
+        print(player_two.dices, " ", p_two_comb, "\n")
+        
+        if player_one.score > player_two.score:
             player_win += 1
-            print(f'{Fore.BLUE}Player win this round!{Style.RESET_ALL}')
+            print(f"{Fore.BLUE}{player_one.name} win this round!{Style.RESET_ALL}")
         else:
             ai_win += 1
-            print(f'{Fore.RED}AI win this round!{Style.RESET_ALL}')
+            print(f"{Fore.RED}{player_two.name} win this round!{Style.RESET_ALL}")
         input("Press enter to continue")
         os.system('clear')
     if player_win > ai_win:
-        player_money += bank
-        print(f'{Fore.BLUE}Player win this game!{Style.RESET_ALL}')
+        player_one.money += bank
+        print(f"{Fore.BLUE}{player_one.name} win this game!{Style.RESET_ALL}")
     else:
-        ai_money += bank
-        print(f'{Fore.RED}AI win this game!{Style.RESET_ALL}')
-    input(f'{Fore.GREEN}Press enter to next game{Style.RESET_ALL}')
-    if player_money <= 0:
-        player_money = 100
-    if ai_money <= 0:
-        ai_money = 100         
+        player_two.money += bank
+        print(f"{Fore.RED}{player_one.name} win this game!{Style.RESET_ALL}")
+    input(f"{Fore.GREEN}Press enter to next game{Style.RESET_ALL}")
+    if player_one.money <= 0:
+        player_one.money = 100
+    if player_two.money <= 0:
+        player_two.money = 100         
     os.system('clear')
-    new_game(player_money, ai_money)
+    new_game(player_one, player_two)
 
-start_menu()
+
+def main():
+    
+    start_menu()
+    
+    
+if __name__ == '__main__':
+    
+    try:
+        main()
+        
+    except KeyboardInterrupt:
+        print()
+        print("Shutting down, bye!")
